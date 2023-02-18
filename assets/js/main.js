79ffd59b6f46b23a031029fbeacb6a0312d2e2cb
Campos Delgado Baltazar <91445820+Baltazar-CD@users.noreply.github.com>
2023-02-17 21:07:30 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@ $(document).ready(function($) {
 	/*=========================================
 		    Countdown
 	=========================================*/
-	$('#counter').countdown('2022/11/28', function (event) {
+	$('#counter').countdown('2023/02/20', function (event) {
 		$('#day').html(event.strftime('%D'));
 		$('#hour').html(event.strftime('%H'));
 		$('#minute').html(event.strftime('%M'));
